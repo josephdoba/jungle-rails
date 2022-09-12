8995d6480a5e49c5102b0f4b3e11569c8c5ae6d5
Joseph Doba <josephdoba@gmail.com>
2022-09-11 19:22:25 -0700

    implement admin dashboard counts

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,7 +2,8 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["USERNAME"], password: ENV["PASSWORD"]
 
   def show
-    @category = Category.find(1)
-    @products = @category.products.order(created_at: :desc)
+    # @category = Category.find(1)
+    # @products = @category.products
+    @products
   end
 end
