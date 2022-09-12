1b84028d69c5b7637794eb8eff8c110c8146ae2a
Joseph Doba <josephdoba@gmail.com>
2022-09-11 19:13:01 -0700

    category and product counts

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["USERNAME"], password: ENV["PASSWORD"]
 
   def show
+    @category = Category.find(1)
+    @products = @category.products.order(created_at: :desc)
   end
 end
