d2107efe385a4739bca7e68fb044009b4bb042ee
Joseph Doba <josephdoba@gmail.com>
2022-09-14 19:39:52 -0700

    implement has_secure_password

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'users/new'
+  post 'users/create'
   get 'about/show'
   root to: 'products#index'
 
@@ -17,6 +19,8 @@ Rails.application.routes.draw do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
   end
+
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
